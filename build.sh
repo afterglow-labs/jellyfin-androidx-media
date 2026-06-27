8bbffe3bc71a2d03802e4f45362224e1c2f2b6d5
afterglow-labs <xuninc@gmail.com>
2026-06-27 17:41:51 -0500

    Update build.sh to include new video decoders
    Added mpeg4 and msmpeg4v3 decoders to the ENABLED_DECODERS array and updated additional configure flags for AVI support.

--- build.sh
+++ build.sh
@@ -9,7 +9,13 @@ export ANDROID_NDK_PATH=$ANDROID_HOME/ndk/26.1.10909125
 export ANDROIDX_MEDIA_ROOT="${PWD}/media"
 export FFMPEG_MOD_PATH="${ANDROIDX_MEDIA_ROOT}/libraries/decoder_ffmpeg/src/main"
 export FFMPEG_PATH="${PWD}/ffmpeg"
-export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd)
+
+# 1. ADDED: Added mpeg4 and msmpeg4v3 video decoders to the array
+export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd mpeg4 msmpeg4v3)
+
+# 2. ADDED: Google's underlying script automatically checks this exact variable name.
+# We use it to force enable the AVI container format and its byte-stream parser.
+export ADDITIONAL_CONFIGURE_FLAGS="--enable-demuxer=avi --enable-parser=mpeg4video"
 
 # Create softlink to ffmpeg
 ln -sf "${FFMPEG_PATH}" "${FFMPEG_MOD_PATH}/jni/ffmpeg"
